9e8fded819a98c27a4f509217145be9c7e575e55
LucasVBoas <contatolucasvilasb@gmail.com>
2024-03-06 21:20:08 -0300

    Aula 2

---
+++ Aula02/src/Cachorro.java
@@ -0,0 +1,15 @@
+public class Cachorro {
+    //Atributo
+    String nome;
+    float peso;
+    int idade;
+    boolean castrado;
+
+    //Métodos
+    void andar(){
+        System.out.println(nome + " andando..");
+
+    }
+
+
+}
